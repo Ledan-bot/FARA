4bb615ad52e601f92140687baca9d4e26e468ae3
Nicholas Ledan <Nickledan5@gmail.com>
2021-10-18 19:55:46 -0400

    Updated Styled for Graph

--- src/components/ResultPage/BalanceSheet.jsx
+++ src/components/ResultPage/BalanceSheet.jsx
@@ -57,6 +57,7 @@ export default function BalanceSheet({ ticker }) {
           },
         ],
       },
+      maintainAspectRatio: false
     };
     return (
       <section className="m-4 flex flex-row py-5 filter drop-shadow-lg shadow-lg">
@@ -78,26 +79,10 @@ export default function BalanceSheet({ ticker }) {
             <li>Net Debt: ${formatter.format(netDebt)}</li>
           </ul>
         </section>
-        <section className="w-3/4">
-          <Bar data={data} options={options} />
+        <section className="w-3/4 ">
+          <Bar data={data} options={options} height={700}/>
         </section>
       </section>
     )
   }
 }
-{/* <section className="m-4 w-60 bg-indigo-800 border-2 border-solid border-indigo-800 border-opacity-70 rounded">
-  <h3 className="text-gray-300">Balance Sheet</h3>
-  <p className="text-gray-300">Cash and Cash equivalents: ${formatter.format(recentBalanceSheet.cashAndCashEquivalents)}</p>
-  <p className="text-gray-300">Cash and Short Term Investments: ${formatter.format(recentBalanceSheet.cashAndShortTermInvestments)}</p>
-  <p className="text-gray-300">Total Current Assets: ${formatter.format(recentBalanceSheet.totalCurrentAssets)}</p>
-  <p className="text-gray-300">Total Non-Current Assets: ${formatter.format(recentBalanceSheet.totalNonCurrentAssets)}</p>
-  <p className="text-gray-300">Intangible Assets: ${formatter.format(recentBalanceSheet.intangibleAssets)}</p>
-  <p className="text-gray-300">Inventory: {formatter.format(recentBalanceSheet.inventory)}</p>
-  <p className="text-gray-300">Account Payable: ${formatter.format(recentBalanceSheet.accountPayables)}</p>
-  <p className="text-gray-300">Tax Payables: ${formatter.format(recentBalanceSheet.deferredRevenue)}</p>
-  <p className="text-gray-300">Common Stock: {formatter.format(recentBalanceSheet.commonStock)}</p>
-  <p className="text-gray-300">Short Term Debt: ${formatter.format(recentBalanceSheet.shortTermDebt)}</p>
-  <p className="text-gray-300">Long Term Debt: ${formatter.format(recentBalanceSheet.longTermDebt)}</p>
-  <p className="text-gray-300">Total Debt: ${formatter.format(recentBalanceSheet.totalDebt)}</p>
-  <p className="text-gray-300">Net Debt: ${formatter.format(recentBalanceSheet.netDebt)}</p>
-</section> */}
