3059c38ed7be4588f2717ce15f2bf4f5be14c5bf
Nicholas Ledan <Nickledan5@gmail.com>
2021-11-02 22:13:28 -0400

    Updated Quantel Routes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ const session = require('express-session');
 const database = require('../database/index.js');
 const { User } = database.models
 const { apiKey } = require('../env/config.js')
-const {redisCache, redisBalanceSheet, redisGeneralInfo, redisKeyMetrics} = require('./caches.js')
+const {redisCache, redisBalanceSheet, redisGeneralInfo, redisKeyMetrics} = require('./caches.js');
+const {getYahooNews, getKeyMetrics, getGeneralInfo, getBlanceSheet} = require('./QuantelAPI.js');
 // --------------------END OF IMPORTS ---------------------------------
 
 const PORT = process.env.PORT || 8666;
@@ -40,105 +41,6 @@ require('./passportConfig')(passport)
 
 // --------------------END OF MIDDLEWARE ---------------------------------
 
-// --------------------HELPER FUNCTIONS ----------------------------------
-
-
-async function getYahooNews(req, res, next) {
-  try {
-    var options = {
-      method: 'GET',
-      url: 'https://mboum-finance.p.rapidapi.com/ne/news',
-      headers: {
-        'x-rapidapi-host': 'mboum-finance.p.rapidapi.com',
-        'x-rapidapi-key': apiKey
-      }
-    };
-    const response = await axios.request(options)
-    const { data } = response
-    let dataString = JSON.stringify(data);
-
-    client.setex(options.url, 86400, dataString)
-    res.send(data)
-  } catch (err) {
-    console.error(err)
-    res.sendStatus(500)
-  }
-}
-
-async function getKeyMetrics({query}, res, next) {
-  const {ticker} = query
-  console.log('TICKER: ', ticker)
-  try {
-    let options = {
-      method: 'GET',
-      url: `https://quantel-io.p.rapidapi.com/key-metrics/${ticker}`,
-      params: {period: 'quarter'},
-      headers: {
-        'x-rapidapi-host': 'quantel-io.p.rapidapi.com',
-        'x-rapidapi-key': apiKey
-      }
-    };
-    const response = await axios.request(options)
-    const { data } = response
-    let dataString = JSON.stringify(data)
-
-    client.setex(ticker, 86400, dataString)
-    res.send(data)
-  } catch (err) {
-    console.log(err)
-    res.sendStatus(500)
-  }
-}
-
-async function getBlanceSheet({query}, res, next) {
-  const { ticker } = query;
-  try {
-    let options = {
-      method: 'GET',
-      url: `https://quantel-io.p.rapidapi.com/balance-sheet-statement/${ticker}`,
-      params: { period: 'quarter' },
-      headers: {
-        'x-rapidapi-host': 'quantel-io.p.rapidapi.com',
-        'x-rapidapi-key': apiKey
-      }
-    };
-
-    const response = await axios.request(options)
-    const { data } = response
-    let dataString = JSON.stringify(data)
-
-    const str = ticker + 'BS'
-    client.setex(str, 86400, dataString)
-    res.send(data)
-  } catch (err) {
-    console.log(err)
-    res.sendStatus(500)
-  }
-}
-
-async function getGeneralInfo({query}, res, next) {
-  const { ticker } = query;
-  try {
-    let options = {
-      method: 'GET',
-      url: `https://quantel-io.p.rapidapi.com/profile/${ticker}`,
-      headers: {
-        'x-rapidapi-host': 'quantel-io.p.rapidapi.com',
-        'x-rapidapi-key': apiKey
-      }
-    };
-    const response = await axios.request(options)
-    const { data } = response
-    let dataString = JSON.stringify(data)
-
-    const str = ticker + 'GI'
-    client.setex(str, 86400, dataString)
-    res.send(data)
-  } catch (err) {
-    console.log(err)
-    res.sendStatus(500)
-  }
-}
 // --------------------START OF ROUTES -----------------------------------
 app.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
