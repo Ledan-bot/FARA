45a685bab9dab256e319cd57d31e192f43e86918
Nicholas Ledan <Nickledan5@gmail.com>
2021-11-02 22:25:34 -0400

    Added outline of About Page

---
+++ src/components/AboutPage/AboutApp.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+
+export default function AboutApp() {
+
+  return (
+    <section>
+      <h1>About AnalysisNow</h1>
+    </section>
+  )
+}
---
+++ src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import Footer from '../Shared/Footer.jsx'
+import Header from '../Shared/Header.jsx'\
+import AboutApp from './AboutApp.jsx';
+import AboutMe from './AboutMe.jsx';
+
+export default function AboutPage() {
+   return (
+     <>
+      <Header />
+      <main>
+        <AboutApp />
+        <AboutMe />
+      </main>
+      <Footer />
+     </>
+   )
+}
